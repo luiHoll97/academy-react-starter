322885e717507cff3ac6396e726568e5304b7a51
Lui H <tech.luiholl@gmail.com>
2022-09-02 20:30:18 +0200

    setup connection to DATABASE_URL using external URL

--- server/db.js
+++ server/db.js
@@ -1,11 +1,19 @@
 const Pool = require("pg").Pool
 
+// variable for url
+const connectionString = process.env.DATABASE_URL
+if (!connectionString) {
+    throw new Error("missing DATABASE_URL Env Var")
+}
 
-const pool = new Pool ({
-user: "academy",
-host: "localhost",
-port: 5432,
-database: "tododb"
+const pool = new Pool({
+    connectionString,
+    ssl: true
 });
 
 module.exports = pool;
+
+// user: "academy",
+// host: "localhost",
+// port: 5432,
+// database: "tododb"
